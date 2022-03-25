2caf58a4af6e87bbeab7da5e9b98a6d52e64bb87
Edwin Olivo <46498127+MrOlivo@users.noreply.github.com>
2022-03-24 19:00:09 -0700

    Remove swagger

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 require("dotenv").config();
 const express = require("express");
-const swaggerUi = require("swagger-ui-express");
-const morganBody = require("morgan-body");
 const cors = require("cors");
 const app = express();
 const dbConnectNoSql = require("./config/mongo");
 const { dbConnectMySQL } = require("./config/mysql");
-const swaggerSpec = require("./docs/swagger");
-const { loggerSlack } = require("./utils/handleLoger");
 app.use(cors());
 app.use(express.json());
 app.use(express.static("storage"));
@@ -15,27 +11,13 @@ app.use(express.static("storage"));
 const engine = process.env.DB_ENGINE || null;
 const port = process.env.PORT || 3000;
 
-morganBody(app, {
-  skip: function (req, res) {
-    return (
-      [403, 404, 409, 401].includes(res.statusCode) || res.statusCode < 400
-    );
-  },
-  stream: loggerSlack,
-});
-
-/**
- * API - Documentation
- */
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
 /**
  * API Rest
  */
 app.use("/api", require("./routes"));
 
 app.listen(port, () =>
-  console.log(`Tu server esta listo por el puerto ${port}`)
+  console.log(`Server is ready on port ${port}`)
 );
 
 /**
@@ -44,9 +26,7 @@ app.listen(port, () =>
 
 if (engine === "mysql") {
   dbConnectMySQL();
-  return;
 }
 if (engine === "nosql") {
   dbConnectNoSql();
-  return;
 }
