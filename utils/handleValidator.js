eca239c480a5ab14b06b1fa10c4b7165bb163451
Edwin Olivo <46498127+MrOlivo@users.noreply.github.com>
2022-03-24 20:50:12 -0700

    Update handleValidator.js

--- utils/handleValidator.js
+++ utils/handleValidator.js
@@ -1,13 +1,11 @@
-const { validationResult } = require('express-validator'); //TODO:
+const { validationResult } = require("express-validator"); //TODO:
 
 const validateResult = (req, res, next) => {
-    try {
-        validationResult(req).throw()
-        return next()
-    } catch (err) {
-        res.status(403)
-        res.send({ errors: err.array() })
-    }
-}
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  next();
+};
 
-module.exports = { validateResult }
+module.exports = { validateResult };
