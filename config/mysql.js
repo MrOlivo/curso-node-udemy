415da8ca4277b3bafc62b7f6fa15d940a4c9a2db
Edwin Olivo <46498127+MrOlivo@users.noreply.github.com>
2022-04-02 11:25:59 -0700

    Update mysql.js

--- config/mysql.js
+++ config/mysql.js
@@ -12,9 +12,9 @@ const sequelize = new Sequelize(database, username, password, {
 const dbConnectMySQL = async () => {
   try {
     await sequelize.authenticate();
-    console.log("MySQL connected");
+    console.log("MySQL connected.");
   } catch (e) {
-    console.log("MySQL ERROR connected", e);
+    console.log("MySQL ERROR: ", e);
   }
 };
 
