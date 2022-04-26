360ffab79ad6ed0844f78de49f6f4d6d7758616a
Edwin Olivo <46498127+MrOlivo@users.noreply.github.com>
2022-04-26 09:20:58 -0600

    Update paginationParams.js

--- config/paginationParams.js
+++ config/paginationParams.js
@@ -1,7 +1,5 @@
-const {PaginationParameters} = require('mongoose-paginate-v2')
+const { PaginationParameters } = require("mongoose-paginate-v2");
 
-const getParams = (req) => {
-    return new PaginationParameters(req).get()
-}
+const getParams = (req) => new PaginationParameters(req).get();
 
-module.exports = getParams
+module.exports = getParams;
